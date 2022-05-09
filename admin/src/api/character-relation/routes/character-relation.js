d12514a15eb05d4a5dc64757cbef10c0b480b47c
Yuval Shpitzer <YuvalS@cet.ac.il>
2022-05-09 11:06:16 +0000

    Deleted admin

--- admin/src/api/character-relation/routes/character-relation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * character-relation router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::character-relation.character-relation');
--- admin/src/api/character-relation/services/character-relation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * character-relation service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::character-relation.character-relation');
