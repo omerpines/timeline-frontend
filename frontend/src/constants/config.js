d91e7c3170d5cbb26ace32946f727381ab8a9188
Alexey Fedotov <yasenfire@gmail.com>
2022-05-31 14:40:10 +0300

    Changed config

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -15,8 +15,8 @@ const config = {
   MAX_INPUT_YEAR: 120000,
   MIN_RANGE: 100,
   MAX_RANGE: 4000,
-  EMAIL: 'office@929.org.il',
-  API: 'http://localhost:1337',
+  EMAIL: 'Omerpines@gmail.com',
+  API: 'https://timelineapi.testing.929.org.il',
   // In percents. Determines the position of the red line
   FOCUS_POINT: 20,
   // In percents. Determines the distance from the red line where a period background starts to fade in/out
